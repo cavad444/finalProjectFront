d9c1d9084b2149fd6010956961026b07d354ce20
rzamirzayev <rzatm@code.edu.az>
2023-09-05 15:56:44 +0400

    added dark theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,29 @@
-import { CssBaseline, Typography } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { Container } from "@mui/material";
+import { useState } from "react";
 
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
